18f93367be6b3a6b70c0f217e2e27055a5f06f30
calimanis-ygor <ygoralvescalimanis@hotmail.com>
2024-03-12 11:32:56 -0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/PageHeader/";
+import styled from "styled-components";
+import Pesquisa from "./components/Pesquisa";
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589);
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      <Header />
+      <Pesquisa />
+    </AppContainer>
+  );
+}
+
+export default App;
